66851a9e6e5068c3ec2218a3a12e0d39b3e3741d
Justin De Burgo <justin.deburgo@aa.com>
2023-05-08 19:09:08 -0500

    button tests
    Signed-off-by: Justin De Burgo <justin.deburgo@aa.com>

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,12 +2,18 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
+import {View} from 'react-native';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+jest.mock('react-native-gesture-handler', () => {
+  return {
+    GestureHandlerRootView: ({children}) => <>{children}</>,
+  };
+});
 
 it('renders correctly', () => {
   renderer.create(<App />);
--- src/components/form/Button.test.tsx
+++ src/components/form/Button.test.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {render, screen, fireEvent} from '@testing-library/react-native';
 import {Text} from 'react-native';
+jest.mock('react-native-vector-icons/MaterialCommunityIcons');
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {Button} from './Button';
 import AppProvider from '@contexts/AppProvider';
+import {IconProps} from 'react-native-vector-icons/Icon';
 
 const onPressMock = jest.fn();
+
 const renderButton = ({buttonProps}: {buttonProps?: any}) =>
   render(
     <AppProvider navRef={undefined}>
@@ -24,5 +28,9 @@ describe('Button', () => {
     renderButton({buttonProps: {leftAdornment}});
     const button = screen.getByText(/hello world/i);
     expect(button).toBeVisible();
+  });
+
+  it('displays icon when left adornment is string', () => {
+    renderButton({buttonProps: {leftAdornment: 'exclamation'}});
   });
 });
